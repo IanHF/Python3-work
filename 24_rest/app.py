542b71f7aedf4f12e9df4801bce273ebcc6aa1eb
Ian Fried <ifried00@stuy.edu>
2019-11-12 09:31:12 -0500

    possible fix for urllib

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_templte
-import urllib3
+from urllib.request import urlopen
 import json
 app  = Flask(__name__)
 
 @app.route("/")
 def root():
-  u = urllib3.urlopen(
+  u = urllib.request.urlopen(
   "https://api.nasa.gov/planetary/apod?api_key=1qfeLB9B1j2icnEPSBgUipgKp2T2zi2SPvjXLYxQ"
   )
   response = u.read()
