5f67a2db640aadfc4ae8ab80593e3c574332ac45
Ian Fried <ifried00@stuy.edu>
2019-11-12 22:35:09 -0500

    polishing stupid errors

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_templte
+from flask import Flask, render_template
+import urllib
 from urllib.request import urlopen
 import json
 app  = Flask(__name__)
 
 @app.route("/")
 def root():
-  u = urllib.request.urlopen(
-  "https://api.nasa.gov/planetary/apod?api_key=1qfeLB9B1j2icnEPSBgUipgKp2T2zi2SPvjXLYxQ"
-  )
+  u = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=1qfeLB9B1j2icnEPSBgUipgKp2T2zi2SPvjXLYxQ")
   response = u.read()
   data = json.loads(response)
   return render_template("index.html", pic = data['url'])
